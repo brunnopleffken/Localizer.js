9f0f45f8bdad9513fe8023719c2b7b2c8b4f06d7
Brunno Pleffken Hosti <brunnopleffken@Brunnos-MacBook-Pro.local>
2016-02-25 19:39:46 -0300

    Examples

---
+++ example/dictionary.js
@@ -0,0 +1,25 @@
+var localizerDictionary = {
+	"pt-br": {
+		"bird": "Pássaro",
+		"cat": "Gato",
+		"dog": "Cachorro",
+		"fish": "Peixe",
+		"lion": "Leão"
+	},
+	"en": {
+		"bird": "Bird",
+		"cat": "Cat",
+		"dog": "Dog",
+		"elephant": "Elephant",
+		"fish": "Fish",
+		"lion": "Lion"
+	},
+	"de": {
+		"bird": "Vogel",
+		"cat": "Katze",
+		"dog": "Hund",
+		"elephant": "Elefant",
+		"fish": "Fische",
+		"lion": "Löwe"
+	}
+};
